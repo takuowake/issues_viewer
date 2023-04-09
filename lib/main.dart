a68b22f7fca5607345549bfb421bfb60456de051
takuowake <takuowake@gmail.com>
2023-04-09 14:07:05 +0900

    github_apiの修正

--- lib/main.dart
+++ lib/main.dart
@@ -6,146 +6,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'models/issue.dart';
 import 'repositories/github_api.dart';
 import 'repositories/github_repository.dart';
-import 'repositories/issues_repository.dart';
 import 'package:http/http.dart' as http;
-
-// // final issuesRepositoryProvider =
-// // Provider((ref) => IssuesRepository(githubRepository: GithubRepository(api: GithubApi())));
-// final githubRepositoryProvider =
-// Provider((ref) => GithubRepository(api: GithubApi()));
-//
-// void main() {
-//   runApp(ProviderScope(child: MyApp()));
-// }
-//
-// class MyApp extends StatefulWidget {
-//   const MyApp({Key? key}) : super(key: key);
-//
-//   @override
-//   _MyAppState createState() => _MyAppState();
-// }
-//
-// class _MyAppState extends State<MyApp> {
-//   // late final IssuesRepository issuesRepository;
-//   late final GithubRepository githubRepository;
-//   int _currentIndex = 0;
-//   int _page = 1;
-//   bool _isLoading = false;
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//     // issuesRepository = IssuesRepository(githubRepository: GithubRepository(api: GithubApi()));
-//     githubRepository = GithubRepository(api: GithubApi());
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Issues Viewer',
-//       home: DefaultTabController(
-//         length: 6,
-//         child: Scaffold(
-//           appBar: AppBar(
-//             title: Column(
-//               crossAxisAlignment: CrossAxisAlignment.start,
-//               children: [
-//                 const Text('Flutter Issues Viewer'),
-//               ],
-//             ),
-//             bottom: TabBar(
-//               tabs: [
-//                 Tab(text: 'すべて'),
-//                 Tab(text: 'p:webview'),
-//                 Tab(text: 'p: shared_preferences'),
-//                 Tab(text: 'waiting for customer response'),
-//                 Tab(text: 'new feature'),
-//                 Tab(text: 'p: share'),
-//               ],
-//               onTap: (index) {
-//                 setState(() {
-//                   _currentIndex = index;
-//                 });
-//               },
-//             ),
-//           ),
-//           body: TabBarView(
-//             children: [
-//               _buildTabView(context, IssueLabel.all),
-//               _buildTabView(context, IssueLabel.pWebView),
-//               _buildTabView(context, IssueLabel.pSharedPreferences),
-//               _buildTabView(context, IssueLabel.waitingForCustomerResponse),
-//               _buildTabView(context, IssueLabel.newFeature),
-//               _buildTabView(context, IssueLabel.pShare),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-//
-//   Widget _buildTabView(BuildContext context, IssueLabel issueLabel) {
-//     List<Issue> _issues = [];
-//     // 以下の行を追加
-//     return FutureBuilder<List<Issue>>(
-//       // future: issuesRepository.getIssues(issueLabel, page: _page),
-//       future: githubRepository.getIssues(issueLabel, page: _page),
-//       builder: (context, snapshot) {
-//         if (snapshot.connectionState == ConnectionState.waiting && !_isLoading) {
-//           return const Center(child: CircularProgressIndicator());
-//         }
-//         if (snapshot.hasData) {
-//           final issues = snapshot.data;
-//           _issues.addAll(issues!);
-//           return ListView.builder(
-//             shrinkWrap: true,
-//             itemCount: (issues?.length ?? 0) + 1,
-//             itemBuilder: (context, index) {
-//               if (index == issues?.length && _isLoading) {
-//                 return const Center(child: CircularProgressIndicator());
-//               } else if (index == issues?.length) {
-//                 return ElevatedButton(
-//                   onPressed: () async {
-//                     setState(() {
-//                       _isLoading = true;
-//                       _page++;
-//                     });
-//                     // final newIssues = await issuesRepository.getIssues(issueLabel, page: _page);
-//                     final newIssues = await githubRepository.getIssues(issueLabel, page: _page);
-//                     setState(() {
-//                       _isLoading = false;
-//                       issues?.addAll(newIssues);
-//                     });
-//                   },
-//                   child: const Text('次へ'),
-//                 );
-//               }
-//               if (index == issues?.length && _isLoading) {
-//                 return const Center(child: CircularProgressIndicator());
-//               }
-//               final issue = issues![index];
-//               return ListTile(
-//                 title: Text(issue.title),
-//                 subtitle: Text('#${issue.number} opened by ${issue.labels}'),
-//               );
-//             },
-//           );
-//         }
-//         if (snapshot.hasError) {
-//           return Center(child: Text('Failed to load issues: ${snapshot.error}'));
-//         }
-//         return Container(
-//           child: Center(
-//             child: Text(
-//               'No data',
-//               style: TextStyle(color: Colors.black),
-//             ),
-//           ),
-//         );
-//       },
-//     );
-//   }
-// }
 
 final githubRepositoryProvider =
 Provider((ref) => GithubRepository(api: GithubApi()));
@@ -162,29 +23,16 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  // late final IssuesRepository issuesRepository;
   late final GithubRepository githubRepository;
-  List<List<Issue>> _issuesList = [
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-  ];
   int _currentIndex = 0;
   int _page = 1;
-  List<bool> _isLoadingList = [
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-  ];
+  bool _isLoading = false;
 
   @override
   void initState() {
     super.initState();
+    // issuesRepository = IssuesRepository(githubRepository: GithubRepository(api: GithubApi()));
     githubRepository = GithubRepository(api: GithubApi());
   }
 
@@ -213,6 +61,7 @@ class _MyAppState extends State<MyApp> {
               ],
               onTap: (index) {
                 setState(() {
+                  _page = 1;
                   _currentIndex = index;
                 });
               },
@@ -220,12 +69,12 @@ class _MyAppState extends State<MyApp> {
           ),
           body: TabBarView(
             children: [
-              _buildTabView(context, IssueLabel.all, 0),
-              _buildTabView(context, IssueLabel.pWebView, 1),
-              _buildTabView(context, IssueLabel.pSharedPreferences, 2),
-              _buildTabView(context, IssueLabel.waitingForCustomerResponse, 3),
-              _buildTabView(context, IssueLabel.newFeature, 4),
-              _buildTabView(context, IssueLabel.pShare, 5),
+              _buildTabView(context, IssueLabel.all),
+              _buildTabView(context, IssueLabel.pWebView),
+              _buildTabView(context, IssueLabel.pSharedPreferences),
+              _buildTabView(context, IssueLabel.waitingForCustomerResponse),
+              _buildTabView(context, IssueLabel.newFeature),
+              _buildTabView(context, IssueLabel.pShare),
             ],
           ),
         ),
@@ -233,11 +82,11 @@ class _MyAppState extends State<MyApp> {
     );
   }
 
-  Widget _buildTabView(BuildContext context, IssueLabel issueLabel, int index) {
-    // 以下の行を変更
-    List<Issue> _issues = _issuesList[index];
-    bool _isLoading = _isLoadingList[index];
+  Widget _buildTabView(BuildContext context, IssueLabel issueLabel) {
+    List<Issue> _issues = [];
+    // 以下の行を追加
     return FutureBuilder<List<Issue>>(
+      // future: issuesRepository.getIssues(issueLabel, page: _page),
       future: githubRepository.getIssues(issueLabel, page: _page),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting && !_isLoading) {
@@ -256,12 +105,13 @@ class _MyAppState extends State<MyApp> {
                 return ElevatedButton(
                   onPressed: () async {
                     setState(() {
-                      _isLoadingList[_currentIndex] = true;
+                      _isLoading = true;
                       _page++;
                     });
+                    // final newIssues = await issuesRepository.getIssues(issueLabel, page: _page);
                     final newIssues = await githubRepository.getIssues(issueLabel, page: _page);
                     setState(() {
-                      _isLoadingList[_currentIndex] = false;
+                      _isLoading = false;
                       issues?.addAll(newIssues);
                     });
                   },
--- lib/repositories/github_api.dart
+++ lib/repositories/github_api.dart
@@ -9,7 +9,7 @@ final githubApiProvider = Provider((ref) => GithubApi());
 class GithubApi {
   static const String _baseUrl = 'https://api.github.com';
   static const String _repository = 'flutter/flutter';
-  final String githubAccessToken = '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';
+  final String githubAccessToken = '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';
   int perPage = 10;
 
   ///全てのIssuesを取得
@@ -42,57 +42,9 @@ class GithubApi {
 
     if (response.statusCode == 200) {
       final List<dynamic> data = json.decode(response.body);
-      if (data == null) {
-        return [];
-      }
       return data.map((json) => Issue.fromJson(json)).toList();
     } else {
       throw Exception('Failed to load issues: ${response.reasonPhrase}');
     }
   }
 }
-
-// final githubApiProvider = Provider((ref) => GithubApi());
-//
-// ///Github APIの呼び出し
-// class GithubApi {
-//   static const String _baseUrl = 'https://api.github.com';
-//   static const String _repository = 'flutter/flutter';
-//   final String githubAccessToken = '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';
-//
-//   ///ページ単位でIssuesを取得
-//   Future<List<Issue>> fetchIssues(int page) async {
-//     final response = await http.get(
-//       Uri.parse('$_baseUrl/repos/$_repository/issues?&per_page=10&state=all'),
-//       // Uri.parse('$_baseUrl/repos/$_repository/issues?&per_page=100&page=$page&state=all'),
-//       headers: {
-//         'Authorization': 'token $githubAccessToken',
-//         'Accept': 'application/vnd.github.v3+json',
-//       },
-//     );
-//
-//     if (response.statusCode == 200) {
-//       final List<dynamic> data = json.decode(response.body);
-//       return data.map((json) => Issue.fromJson(json)).toList();
-//     } else {
-//       throw Exception('Failed to load issues: ${response.reasonPhrase}');
-//     }
-//   }
-//
-//   ///全てのIssuesを取得
-//   Future<List<Issue>> fetchAllIssues() async {
-//     List<Issue> allIssues = [];
-//     int page = 1;
-//
-//     while (true) {
-//       final List<Issue> issues = await fetchIssues(page);
-//       if (issues.isEmpty) {
-//         break;
-//       }
-//       allIssues.addAll(issues);
-//       page++;
-//     }
-//
-//     return allIssues;
-//   }
-// }
